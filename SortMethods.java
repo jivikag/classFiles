6daaf3682afbe7429b2f7ddd7e7cc2a8490940a4
Jivika <82114811+jivikag@users.noreply.github.com>
2024-01-16 09:49:06 -0800

    Add files via upload

--- SortMethods.java
+++ SortMethods.java
@@ -1,127 +1,89 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;		// for testing purposes
+
 /**
- *	SortMethods - Sorts an array of Integers in ascending order.
+ *	SortMethods - Sorts an ArrayList of Strings in ascending order.
  *
- *	@author Jivika Gulrajani
- *	@since	November 30, 2023
+ *	Requires FileUtils class to compile.
+ *	Requires file randomWords.txt to execute a test run.
+ *
+ *	@author	
+ *	@since	
  */
 public class SortMethods {
 	
 	/**
-	 *	Bubble Sort algorithm - in ascending order
-	 *	@param arr		array of Integer objects to sort
+	 *	Merge Sort algorithm - in ascending order
+	 *	@param arr		List of String objects to sort
 	 */
-	public void bubbleSort(Integer [] arr) {
-		
-		for (int outer = arr.length-1; outer > 0; outer--) {
-			for (int inner = 0; inner < outer; inner++) {
-				if (arr[inner].compareTo(arr[inner+1]) > 0) {
-					swap(arr, inner, inner + 1);
-				}
+	public void mergeSort(List<String> arr) {
+		mergeSortRecurse(arr, 0, arr.size() - 1);
+	}
+	
+	/**
+	 *	Recursive mergeSort method.
+	 *	@param arr		List of String objects to sort
+	 *	@param first	first index of arr to sort
+	 *	@param last		last index of arr to sort
+	 */
+	public void mergeSortRecurse(List<String> arr, int first, int last) {
+		if (end - start < 2) {
+			if (end > start && cities.get(end).compareTo(cities.get(start)) > 0) {
+				swap(start, end);
 			}
 		}
-	}
-	
-	/**
-	 *	Swaps two Integer objects in array arr
-	 *	@param arr		array of Integer objects
-	 *	@param x		index of first object to swap
-	 *	@param y		index of second object to swap
-	 */
-	private void swap(Integer[] arr, int x, int y) {
-		Integer temp = arr[x];
-		arr[x] = arr[y];
-		arr[y] = temp;
-	}
-	
-	/**
-	 *	Selection Sort algorithm - in ascending order (you implement)
-	 *	@param arr		array of Integer objects to sort
-	 */
-	public void selectionSort(Integer [] arr) {
-		//double for loop
-		//37 minutes
-		for (int i = 0; i < arr.length; i++) {
-			
+		
+		else {
+			int mid = (start + end) / 2;
+			mSort(start, mid);
+			mSort(mid + 1, end);
+			merge(start, mid, end);
 		}
 	}
 	
 	/**
-	 *	Insertion Sort algorithm - in ascending order (you implement)
-	 *	@param arr		array of Integer objects to sort
+	 *	Merge two lists that are consecutive elements in array.
+	 *	@param arr		List of String objects to merge
+	 *	@param first	first index of first list
+	 *	@param mid		the last index of the first list;
+	 *					mid + 1 is first index of second list
+	 *	@param last		last index of second list
 	 */
-	public void insertionSort(Integer [] arr) {
-		//for loop, while loop
+	public void merge(List<String> arr, int first, int mid, int last) {
+		// Insert your code here
 	}
+
 	
 	/**
-	 *	Merge Sort algorithm - in ascending order (you implement)
-	 *	@param arr		array of Integer objects to sort
+	 *	Print an List of Strings to the screen
+	 *	@param arr		the List of Strings
 	 */
-	public void mergeSort(Integer [] arr) {}
-	
-	/*****************************************************************/
-	/************************* For Testing ***************************/
-	/*****************************************************************/
-	
-	/**
-	 *	Print an array of Integers to the screen
-	 *	@param arr		the array of Integers
-	 */
-	public void printArray(Integer[] arr) {
-		if (arr.length == 0) System.out.print("(");
-		else System.out.printf("( %4d", arr[0]);
-		for (int a = 1; a < arr.length; a++) {
-			if (a % 10 == 0) System.out.printf(",\n  %4d", arr[a]);
-			else System.out.printf(", %4d", arr[a]);
+	public void printArray(List<String> arr) {
+		if (arr.size() == 0) System.out.print("(");
+		else System.out.printf("( %-15s", arr.get(0));
+		for (int a = 1; a < arr.size(); a++) {
+			if (a % 5 == 0) System.out.printf(",\n  %-15s", arr.get(a));
+			else System.out.printf(", %-15s", arr.get(a));
 		}
 		System.out.println(" )");
 	}
-
+	
+	/*************************************************************/
+	/********************** Test program *************************/
+	/*************************************************************/
+	private final String FILE_NAME = "randomWords.txt";
+	
 	public static void main(String[] args) {
 		SortMethods se = new SortMethods();
 		se.run();
 	}
 	
 	public void run() {
-		Integer[] arr = new Integer[10];
-		// Fill arr with random numbers
-		for (int a = 0; a < 10; a++)
-			arr[a] = (int)(Math.random() * 100) + 1;
-		System.out.println("\nBubble Sort");
-		System.out.println("Array before sort:");
-		printArray(arr);
-		System.out.println();
-		bubbleSort(arr);
-		System.out.println("Array after sort:");
-		printArray(arr);
-		System.out.println();
-/*		
-		for (int a = 0; a < 10; a++)
-			arr[a] = (int)(Math.random() * 100) + 1;
-		System.out.println("\nSelection Sort");
-		System.out.println("Array before sort:");
-		printArray(arr);
-		System.out.println();
-		selectionSort(arr);
-		System.out.println("Array after sort:");
-		printArray(arr);
-		System.out.println();
-*/
-/*		
-		for (int a = 0; a < 10; a++)
-			arr[a] = (int)(Math.random() * 100) + 1;
-		System.out.println("\nInsertion Sort");
-		System.out.println("Array before sort:");
-		printArray(arr);
-		System.out.println();
-		insertionSort(arr);
-		System.out.println("Array after sort:");
-		printArray(arr);
-		System.out.println();
-*/
-/*		
-		for (int a = 0; a < 10; a++)
-			arr[a] = (int)(Math.random() * 100) + 1;
+		List<String> arr = new ArrayList<String>();
+		// Fill List with random words from file		
+		fillArray(arr);
+		
 		System.out.println("\nMerge Sort");
 		System.out.println("Array before sort:");
 		printArray(arr);
@@ -130,6 +92,13 @@ public class SortMethods {
 		System.out.println("Array after sort:");
 		printArray(arr);
 		System.out.println();
-*/
+	}
+	
+	// Fill String array with words
+	public void fillArray(List<String> arr) {
+		Scanner inFile = FileUtils.openToRead(FILE_NAME);
+		while (inFile.hasNext())
+			arr.add(inFile.next());
+		inFile.close();
 	}
 }
